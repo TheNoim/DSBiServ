2469fcac39d2bc2390edc354c84b4b7831696059
Noim <nilsbergmann@noim.io>
2017-01-15 21:45:06 +0100

    Update

--- dsb.js
+++ dsb.js
@@ -197,10 +197,17 @@ function parseThisShittyIframeAgain(Src, callback){
                             if (TheLastLink){
                                 request(TheLastLink, (error, response, body) => {
                                     if (!error && response.statusCode == 200) {
-                                        console.log(typeof body);
+                                        /**
+                                         * @author http://stackoverflow.com/users/1537042/ofir-luzon, http://stackoverflow.com/users/402884/chris-martin
+                                         * @type {RegExp}
+                                         */
+                                        const regexForDate = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})/;
+
                                         const $ = cheerio.load(body);
                                         const J = $(".mon_title");
-                                        console.log(J.contents()[0].data);
+                                        const date = regexForDate.exec(J.contents()[0].data)[0];
+                                        console.log(`Found: ${date}`);
+
                                         fs.writeFile(`${__dirname}/${sanitize(J.contents()[0].data)}.html`, body, () => {
                                             callback();
                                         });
