99f13ae4ada5a18efd5ae200f1752b5dad6ea441
Noim <nilsbergmann@noim.io>
2017-01-16 20:26:38 +0100

    Update

--- dsb.js
+++ dsb.js
@@ -1,235 +1,273 @@
-const async = require('async');
+/**
+ * Created by nilsbergmann on 16.01.17.
+ */
+
 const request = require('request');
 const fs = require('fs');
 const cheerio = require('cheerio');
-const XRegExp = require('xregexp');
-const sanitize = require("sanitize-filename");
+const _ = require('lodash');
+const async = require('async');
 
-let cookies = null;
-try {
-    cookies = require('./cookie_cache.json');
-} catch (e) {
-    console.log("No cookie cache or invalid.");
-}
+class DSBLibrary {
 
-let config = null;
-try {
-    config = require('./config.json');
-} catch (e) {
-    console.log("Config invalid or does not exists.");
-    process.exit(1);
-}
-
-function login(callback) {
-    request.post(`https://${config.host}/iserv/login_check`, {
-        form: {
-            _username: config.username,
-            _password: config.password
+    /**
+     *
+     * @param {string} iServHost
+     * @param {string} iServUsername
+     * @param {string} iServPassword
+     * @param {string} CookieFile - Path to file with cookies (iServ login cookie cache)
+     * @param {boolean} [debugOutput]
+     */
+    constructor(iServHost, iServUsername, iServPassword, CookieFile ,debugOutput){
+        this.host = iServHost;
+        this.username = iServUsername;
+        this.password = iServPassword;
+        this.debug = debugOutput ? debugOutput : false;
+        this.log = function (message) {
+            if (this.debug){
+                console.log(message);
+            }
+        };
+        this.cookies = null;
+        this.cookie_cache = CookieFile ? CookieFile : null;
+        try {
+            this.cookies = require(CookieFile);
+        } catch (e) {
+            this.cookies = null;
         }
-    }, (error, response, body) => {
-        if (!error && response.statusCode == 302) {
-            if (response.headers["set-cookie"]){
-                cookies = {};
-                for (let u in response.headers["set-cookie"]){
-                    const current = response.headers["set-cookie"][u];
-                    const m = /^[^;]+/.exec(current);
-                    if (m){
-                        const CookieSplit = m[0].split("=");
-                        if (CookieSplit[0] && CookieSplit[1]){
-                            cookies[CookieSplit[0]] = CookieSplit[1];
+    }
+
+    getDSBPlans(DSBCallback){
+        async.waterfall([
+            (WaterfallCallback) => {
+                if (this.cookies){
+                    this._checkCookies((error) => {
+                        if (error){
+                            this._login(WaterfallCallback);
+                        } else {
+                            WaterfallCallback();
+                        }
+                    });
+                } else {
+                    this._login(WaterfallCallback);
+                }
+            },
+            (WaterfallCallback) => this._DoRequest(`https://${this.host}/iserv/plan/show/raw/DSB%20Schueler`, null, false, WaterfallCallback),
+            (HTML, WaterfallCallback) => WaterfallCallback(null, this._FastIFrameParse(HTML, 'iframe[name="DSBHPLehrer"]')),
+            (URL, WaterfallCallback) => this._DoRequest(URL, null, true, WaterfallCallback),
+            (HTML, URLReferer, WaterfallCallback) => WaterfallCallback(null, this._FastIFrameParse(HTML, 'iframe'), URLReferer),
+            (URL, URLReferer, WaterfallCallback) => this._DoRequest(URL, URLReferer, false, WaterfallCallback),
+            (HTML, WaterfallCallback) => {
+                /**
+                 * Now lets get both plans: Vertretungsplan-Modul, Vertretungsplan-Modul 2
+                 */
+                let Plans = [];
+                async.each(this._FastIFrameParse(HTML, ['iframe[name="Vertretungsplan-Modul"]','iframe[name="Vertretungsplan-Modul 2"]']), (IFrame, EachCallback) => {
+                    const URL = IFrame.src;
+                    if (URL){
+                        this._DoRequest(URL, null, true, (error, HTML, Referer) => {
+                            if (error) {
+                                EachCallback(error);
+                            } else {
+                                const PlanURL = this._GetThisShittyJSLocationHref(HTML);
+                                this._DoRequest(PlanURL, Referer, false, (error, HTML) => {
+                                    this._DoRequest(this._FastIFrameParse(HTML, 'iframe'), PlanURL, false, (error, HTML) => {
+                                        if (error){
+                                            EachCallback(error);
+                                        } else {
+                                            Plans.push(HTML);
+                                            EachCallback();
+                                        }
+                                    });
+                                });
+                            }
+                        });
+                    } else {
+                        EachCallback();
+                    }
+
+                }, (error) => {
+                    WaterfallCallback(error, Plans);
+                });
+            }
+        ], (error, Plans) => {
+            if (error) this.log(error);
+            DSBCallback(error, Plans);
+        });
+    }
+
+    /**
+     * Login into iServ
+     * @param LoginCallback
+     * @private
+     */
+    _login(LoginCallback){
+        this.log(`[DEBUG] Start login process.`);
+        /**
+         * Send login request
+         */
+        request.post(`https://${this.host}/iserv/login_check`, {
+            form: {
+                _username: this.username,
+                _password: this.password
+            }
+        }, (error, response) => {
+            if (!error && response.statusCode == 302) {
+                if (response.headers["set-cookie"]){
+                    let cookies = {};
+                    /**
+                     * Parse the 'set-cookie' to a use able format.
+                     */
+                    for (let u in response.headers["set-cookie"]){
+                        if (response.headers["set-cookie"].hasOwnProperty(u)){
+                            const current = response.headers["set-cookie"][u];
+                            const m = /^[^;]+/.exec(current);
+                            if (m){
+                                const CookieSplit = m[0].split("=");
+                                if (CookieSplit[0] && CookieSplit[1]){
+                                    cookies[CookieSplit[0]] = CookieSplit[1];
+                                }
+                            }
                         }
                     }
-                }
-                fs.writeFile(`${__dirname}/cookie_cache.json`, JSON.stringify({
-                    cookies: cookies
-                }), (err) => {
-                    if (err){
-                        console.error(err);
-                        process.exit(1);
+                    this.cookies = cookies;
+                    this._makeCookieHeaderString();
+                    this.log(`[DEBUG] Login successfully with user ${this.username}!`);
+                    if (this.cookie_cache){
+                        fs.writeFile(this.cookie_cache, JSON.stringify(this.cookies), LoginCallback);
+                    } else {
+                        LoginCallback();
                     }
-                    callback();
-                });
-            } else {
-                console.error(`No cookies. No session.`);
-                process.exit(1);
-            }
-        } else {
-            console.error(`Something went wrong. ${error} ${JSON.stringify(response)}`);
-            process.exit(1);
-        }
-    });
-}
-let cookieString;
-
-async.waterfall([
-    (WCallback) => {
-        if (cookies){
-            console.log("Test session...");
-            let cookieString = "";
-            for (let CookieIndex in cookies.cookies){
-                if (cookies.cookies.hasOwnProperty(CookieIndex)) {
-                    cookieString = cookieString + `${CookieIndex}=${cookies.cookies[CookieIndex]}; `;
+                } else {
+                    /**
+                     * With the header 'set-cookie' sets iServ the login sessions. If there is no header 'set-cookie' this error appears.
+                     */
+                    LoginCallback(`No 'set-cookie' found. Login not successfully ?`);
                 }
+            } else {
+                /**
+                 * The response status code needs to be 302 if not this error appears.
+                 */
+                LoginCallback(`Something went wrong. Response code: ${response.statusCode} | Error: ${error}`);
             }
-            request(`https://${config.host}/iserv/login_check`, {
+        });
+    }
+
+    /**
+     * Checks if the cookies are still valid
+     * @param CookieCallback
+     * @private
+     */
+    _checkCookies(CookieCallback){
+        this.log(`[DEBUG] Check if your cookies are still valid.`);
+        if (this.cookies){
+            /**
+             * Header cookie string from cookies.
+             * @type {string}
+             */
+            this._makeCookieHeaderString();
+            request(`https://${this.host}/iserv/login_check`, {
                 headers: {
-                    Cookie: cookieString
+                    Cookie: this.cookieHeader
                 },
                 followRedirect: false
             },(error, response, body) => {
                 if (!error && response.statusCode == 200) {
-                    WCallback();
+                    CookieCallback();
                 } else {
-                    login(WCallback);
+                    CookieCallback(`It seems that your cookies are no longer valid! Response status code ${response.statusCode} | Error ${error}`);
                 }
             });
         } else {
-            login(WCallback);
+            CookieCallback(`Where are my cookies !!?? This should never happen!`);
         }
-    },
-    (WCallback) => {
-        console.log("Login successfully");
-        cookieString = "";
-        for (let CookieIndex in cookies){
-            if (cookies.hasOwnProperty(CookieIndex)) {
-                cookieString = cookieString + `${CookieIndex}=${cookies[CookieIndex]}; `;
-            }
-        }
-        //console.log(cookieString);
-        request(`https://${config.host}/iserv/plan/show/raw/DSB%20Schueler`, {
-            headers: {
-                Cookie: cookieString
-            }
-        }, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                WCallback(null, body, cookieString);
-            } else {
-                console.error(error);
-                process.exit(1);
-            }
-        })
-    },
-    (body, CookieString, WCallback) => {
-        //console.log(body);
-        const $ = cheerio.load(body);
-        const src = $('iframe[name="DSBHPLehrer"]').attr('src');
-        if (src){
-            WCallback(null, src, CookieString);
-        } else {
-            process.exit(1);
-        }
-    },
-    (NextSrc,CookieString , WCallback) => {
-        request(NextSrc, {
-            headers: {
-                Cookie: CookieString
-            }
-        }, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                WCallback(null, body, NextSrc);
-            } else {
-                console.error(error);
-                process.exit(1);
-            }
-        });
-    },
-    (Body, NextSrc , WCallback) => {
-        const $ = cheerio.load(Body);
-        const src = $('iframe').attr('src');
-        if (src){
-            WCallback(null, src, NextSrc);
-        } else {
-            process.exit(1);
-        }
-    },
-    (NextSrc, Referer, WCallback) => {
-        request(NextSrc, {
-            headers: {
-                Cookie: cookieString,
-                Referer: Referer
-            }
-        }, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                WCallback(null, body);
-            } else {
-                console.error(error);
-                process.exit(1);
-            }
-        });
-    },
-    (body, WCallback) => {
-        //console.log(body);
-        const $ = cheerio.load(body);
-        const src1 = $('iframe[name="Vertretungsplan-Modul 2"]').attr('src');
-        const src2 = $('iframe[name="Vertretungsplan-Modul"]').attr('src');
-        WCallback(null, src1, src2);
-    },
-    (Src1, Src2, WCallback) => {
-         parseThisShittyIframeAgain(Src1, () => {
-             parseThisShittyIframeAgain(Src2, () => {
-                 WCallback();
-             });
-         });
     }
-],() => {
-    process.exit(1);
-});
 
+    /**
+     * Make cookie header string
+     * @private
+     */
+    _makeCookieHeaderString(){
+        let cookieString = "";
+        for (let CookieIndex in this.cookies){
+            if (this.cookies.hasOwnProperty(CookieIndex)) {
+                cookieString = cookieString + `${CookieIndex}=${this.cookies[CookieIndex]}; `;
+            }
+        }
+        this.cookieHeader = cookieString;
+    }
 
-function parseThisShittyIframeAgain(Src, callback){
-    if (!Src){
-        callback();
-    } else {
-        request(Src, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                const superRegex = /location.href=".+(?=";)/;
-                const m = superRegex.exec(body);
-                const src = m[0].replace('location.href="', '');
-                if (src){
-                    request(src, {
-                        headers: {
-                            Referer: Src
-                        }
-                    },(error, response, body) => {
-                        if (!error && response.statusCode == 200) {
-                            const $ = cheerio.load(body);
-                            const TheLastLink = $('iframe').attr('src');
-                            if (TheLastLink){
-                                request(TheLastLink, (error, response, body) => {
-                                    if (!error && response.statusCode == 200) {
-                                        /**
-                                         * @author http://stackoverflow.com/users/1537042/ofir-luzon, http://stackoverflow.com/users/402884/chris-martin
-                                         * @type {RegExp}
-                                         */
-                                        const regexForDate = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})/;
-
-                                        const $ = cheerio.load(body);
-                                        const J = $(".mon_title");
-                                        const date = regexForDate.exec(J.contents()[0].data)[0];
-                                        console.log(`Found: ${date}`);
-
-                                        fs.writeFile(`${__dirname}/${sanitize(J.contents()[0].data)}.html`, body, () => {
-                                            callback();
-                                        });
-                                    } else {
-                                        process.exit(1);
-                                    }
-                                });
-                            } else {
-                                process.exit(1);
-                            }
-                        } else {
-                            process.exit(1);
-                        }
+    /**
+     * Fast parse the html and get the src of a iFrame with given selector
+     * @param HTML
+     * @param {Array|string} Selector
+     * @returns {Array|jQuery}
+     * @private
+     */
+    _FastIFrameParse(HTML, Selectors){
+        const $ = cheerio.load(HTML);
+        if (_.isArray(Selectors)){
+            let returnArray = [];
+            for (let index in Selectors){
+                if (Selectors.hasOwnProperty(index)){
+                    returnArray.push({
+                        selector: Selectors[index],
+                        src: $(Selectors[index]).attr('src')
                     });
-                } else {
-                    console.log("IS NULKLLLLLLL");
-                    process.exit(1);
                 }
+            }
+            return returnArray;
+        } else {
+            return $(Selectors).attr('src');
+        }
+
+    }
+
+    /**
+     * Fast do a request and handel errors
+     * @param URL
+     * @param Referer - The on going url to fake a iFrame request
+     * @param WithURL
+     * @param RequestCallback
+     * @private
+     */
+    _DoRequest(URL, Referer, WithURL, RequestCallback){
+        let options = {
+            headers: {
+                Cookie: this.cookieHeader
+            }
+        };
+        if (Referer){
+            options.headers.Referer = Referer;
+        }
+        this.log(`[DEBUG] Do request to ${URL} with this options: ${JSON.stringify(options)}`);
+        request(URL, options, (error, response, body) => {
+            if (!error && response.statusCode == 200){
+                this.log(`[DEBUG] Successfully!`);
+                if (WithURL){
+                    RequestCallback(null, body, URL);
+                } else {
+                    RequestCallback(null, body);
+                }
+
             } else {
-                console.error(error);
-                process.exit(1);
+                RequestCallback(`Hmm. Something happened. Maybe you know what to do with this status code ${response.statusCode} and this error ${error}`);
             }
         });
     }
+
+    /**
+     * Parse the html and fast get this shitty location.href url
+     * @param HTML
+     * @returns {XML|*|void|String|string}
+     * @private
+     */
+    _GetThisShittyJSLocationHref(HTML) {
+        const superRegex = /location.href=".+(?=";)/;
+        const m = superRegex.exec(HTML);
+        return m[0].replace('location.href="', '');
+    }
+
 }
+
+module.exports = DSBLibrary;
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 /**
  * Created by nilsbergmann on 16.01.17.
  */
-const DSBLibrary = require('./dsblib');
+const DSBLibrary = require('./dsb');
 const dsb = new DSBLibrary(require('./config.json').host, require('./config.json').username, require('./config.json').password, `./cookie_cache.json`, true);
 dsb.getDSBPlans(console.log);
