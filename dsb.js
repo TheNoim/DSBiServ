84ca68c8bd3f3f0a76944463ec443599f31f6bf5
Noim <nilsbergmann@noim.io>
2017-03-28 20:37:54 +0200

    Fix for mcg

--- dsb.js
+++ dsb.js
@@ -122,7 +122,7 @@ class DSBLibrary {
                                             const $ = cheerio.load(HTML);
                                             const MonTitle = $(".mon_title");
                                             const regexForDate = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})/;
-                                            const date = regexForDate.exec(MonTitle.contents()[0].data)[0];
+                                            const date = regexForDate.exec(MonTitle.contents()[0].data.split(' ')[0])[0];
                                             Plans.push({
                                                 date: date,
                                                 html: HTML
